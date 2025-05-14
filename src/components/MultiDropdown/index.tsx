ba412a9831e01f8c0dca50175a9cf2f3169fc3a5
adadvar <a.dadvar_71>
2025-05-14 20:45:12 +0330

    convert to controlled component with input

--- src/components/MultiDropdown/index.tsx
+++ src/components/MultiDropdown/index.tsx
@@ -1,12 +1,19 @@
 import styles from "./styles.module.scss";
+import { useState } from "react";
 import DropdownList from "./DropdownList";
 
 function MultiDropdown() {
   const items = ["item1", "item2"];
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <div className="multi-dropdown">
       <div className="dropdown-input">
-        <input type="text" />
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
         <button>+</button>
       </div>
       <DropdownList items={items} />

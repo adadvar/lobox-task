5c53438624e504d70bdcaa6ddce0b43a41ac72d6
adadvar <a.dadvar_71>
2025-05-14 20:51:44 +0330

    handle input when enter so add item to items

--- src/components/MultiDropdown/index.tsx
+++ src/components/MultiDropdown/index.tsx
@@ -3,8 +3,17 @@ import { useState } from "react";
 import DropdownList from "./DropdownList";
 
 function MultiDropdown() {
-  const items = ["item1", "item2"];
+  const [items, setItems] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState("");
+  const handleAddItem = (e: React.KeyboardEvent) => {
+    if (
+      e.key === "Enter" &&
+      inputValue.trim() &&
+      !items.includes(inputValue.trim())
+    ) {
+      setItems([...items, inputValue.trim()]);
+    }
+  };
 
   return (
     <div className="multi-dropdown">
@@ -13,6 +22,7 @@ function MultiDropdown() {
           type="text"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleAddItem}
         />
         <button>+</button>
       </div>
